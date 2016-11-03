0bdc377347ad83931ca8b07c800248a98ecd77f3
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-11-03 17:03:09 +0100

    moved the non-optimization flags to cabal.project
    during devel we should always build without optimization, since some
    examples require quite a lot of build time

--- ADP/Fusion/Forest/Align/RL.hs
+++ ADP/Fusion/Forest/Align/RL.hs
@@ -2,18 +2,18 @@
 module ADP.Fusion.Forest.Align.RL
   ( module ADP.Fusion.Core
   , module ADP.Fusion.Core.ForestAlign.RightLinear
-  , module ADP.Fusion.Term.Node.ForestAlign.RightLinear
-  , module ADP.Fusion.Term.Epsilon.ForestAlign.RightLinear
-  , module ADP.Fusion.Term.Deletion.ForestAlign.RightLinear
-  , module ADP.Fusion.Term.Node.Type
   , module ADP.Fusion.SynVar.Indices.ForestAlign.RightLinear
+  , module ADP.Fusion.Term.Deletion.ForestAlign.RightLinear
+  , module ADP.Fusion.Term.Epsilon.ForestAlign.RightLinear
+  , module ADP.Fusion.Term.Node.ForestAlign.RightLinear
+  , module ADP.Fusion.Term.Node.Type
   ) where
 
 import ADP.Fusion.Core
 import ADP.Fusion.Core.ForestAlign.RightLinear
-import ADP.Fusion.Term.Node.ForestAlign.RightLinear
-import ADP.Fusion.Term.Epsilon.ForestAlign.RightLinear
-import ADP.Fusion.Term.Deletion.ForestAlign.RightLinear
-import ADP.Fusion.Term.Node.Type
 import ADP.Fusion.SynVar.Indices.ForestAlign.RightLinear
+import ADP.Fusion.Term.Deletion.ForestAlign.RightLinear
+import ADP.Fusion.Term.Epsilon.ForestAlign.RightLinear
+import ADP.Fusion.Term.Node.ForestAlign.RightLinear
+import ADP.Fusion.Term.Node.Type
 
